e5a374234fd5c0ccf3aa35982d116ce72e6638fe
bmiller25 <benjaminjonathanmiller@outlook.com>
2022-02-11 11:39:44 -0800

    Add website files
    These are the website files that will be served by the GitHub Pages server. The website has just one page, main.html.

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    document.getElementById("toradocu_download").onclick = downloadProject("toradocu.zip")
+    document.getElementById("unix_like_kernel_download").onclick = downloadProject("unix-like-kernel.zip")
+    document.getElementById("pet_adoption_app_download").onclick = downloadProject("pet-adoption-app.zip")
+    document.getElementById("othello_download").onclick = downloadProject("othello.zip")
+}
+
+function downloadProject(fileName) {
+    return function() {
+        window.open("Projects/" + fileName);
+    }
+}
