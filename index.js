95c8688a59e8da48a156cc41f2fc5a449baa43b6
Benjamin J. Miller <benjaminjmiller217@yahoo.com>
2023-01-08 18:30:30 -0800

    Removed unnecessary project download

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 window.onload = function() {
-    document.getElementById("toradocu_download").onclick = downloadProject("toradocu.zip")
     document.getElementById("unix_like_kernel_download").onclick = downloadProject("unix-like-kernel.zip")
     document.getElementById("pet_adoption_app_download").onclick = downloadProject("pet-adoption-app.zip")
 }
